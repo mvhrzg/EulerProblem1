a0dc51f4599c34cc190e11dbe73fe80e6e1c8d1d
M. Herzog <mherzog@unca.edu>
2015-02-12 16:19:39 -0500

    O(n) solution

---
+++ EulerProblem1_C.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   EulerProblem1_C.cpp
+ * Author: M. Herzog
+ *
+ * Created on January 27, 2015, 1:15 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int sum = 0;
+    for(int i = 2; i < 1000; i++){
+        if(i%3 == 0 || i%5 == 0){
+            sum += i;
+        }
+    }
+    
+    cout <<"Sum: " << sum << endl;
+
+    return 0;
+}
+
