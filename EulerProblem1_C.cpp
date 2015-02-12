5d4aab3d01012d0341b742f0ab67d006008885bc
M. Herzog <mherzog@unca.edu>
2015-02-12 17:13:32 -0500

    O(1) with user input

--- EulerProblem1_C.cpp
+++ EulerProblem1_C.cpp
@@ -1,28 +1,24 @@
-/* 
- * File:   EulerProblem1_C.cpp
- * Author: M. Herzog
- *
- * Created on January 27, 2015, 1:15 PM
- */
-
 #include <cstdlib>
 #include <iostream>
-
+#include <stdio.h>
+ 
+ using namespace std;
+ 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+ int main(int argc, char** argv) {
     int sum = 0;
-    for(int i = 2; i < 1000; i++){
-        if(i%3 == 0 || i%5 == 0){
-            sum += i;
-        } 
-    }
+    int n;
+    cout << "Enter a number: " << endl;
+    scanf("%d", &n);
+    printf("You entered: %d\n", n);
+    int threes = 3*((n-1)/3) * (1+(n-1)/3)/2;
+    int fives = 5*((n-1)/5) * (1+(n-1)/5)/2;
+    int fifteen = 15*((n-1)/15) * (1+(n-1)/15)/2;
     
+    sum = threes + fives - fifteen;
+     
     cout <<"Sum: " << sum << endl;
-
+ 
     return 0;
-}
-
+ }
